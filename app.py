12b892655c3629d2b55d6be82f4901a02b62dafd
Sunny <bhardwajsunny550@gmail.com>
2024-04-12 15:55:45 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        sentiment=request.form.get('Sentiment')
+        sid =  SentimentIntensityAnalyzer()
+        score =sid.polarity_scores(sentiment)
+
+        if score['neg'] >score['pos'] and score['neu']:
+            return render_template("home.html",message='Negative')
+        elif score['pos'] >score['neg'] and score['neu']:
+            return render_template("home.html",message='Positive')
+        else:
+            return render_template("home.html",message='Neutrel')
+        
+    return render_template("home.html")    
+
+if __name__ == '__main__':
+    app.run(debug=False , host='0.0.0.0')
+
+
